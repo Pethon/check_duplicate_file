72f3b2bda248794a29ed56875bcee4194a8e7d43
Pethon <pasin_th@hotmail.com>
2018-10-18 11:11:41 +0900

    add non-submission student number checker

--- check_duplicate_file.ps1
+++ check_duplicate_file.ps1
@@ -1,7 +1,8 @@
 param (
 	[string]$dir = $( Read-Host "Input directory (ex. C:\kadai\)" ),
 	[int]$num = $( Read-Host "Input file number (ex. 1)" ),
-	[string]$type = $( Read-Host "Input file type (ex. .pdf,.doc,.docx)" )
+	[string]$type = $( Read-Host "Input file type (ex. .pdf,.doc,.docx)" ),
+	[int]$std = $( Read-Host "Input student number (ex. 138)" )
 )
 
 $num = $num + 1
@@ -38,4 +39,32 @@ for ($i=0; $i -lt $files.Count; $i++){
 		echo $files[$i]
 	}
 }
+
+echo ''
+echo '================================'
+echo ''
+echo 'NON-SUBMISSION STUDENT NUMBER'
+echo ''
+
+$index = 0
+if([int]($files[0].Substring(4,3)) -ne 1){
+	for ($j=1; $j -lt [int]($files[1].Substring(4,3)); $j++){
+		$n = $files[0].Substring(0,4) + ("{0:D3}" -f $j)
+		echo $n
+	}
+}	
+for ($i=1; $i -lt $files.Count; $i++){
+	if([int]($files[$i].Substring(4,3))-[int]($files[$i-1].Substring(4,3)) -gt 1){
+		for ($j=[int]($files[$i-1].Substring(4,3))+1; $j -lt [int]($files[$i].Substring(4,3)); $j++){
+			$n = $files[$i].Substring(0,4) + ("{0:D3}" -f $j)
+			echo $n
+		}
+	}
+}
+if([int]($files[$files.Count-1].Substring(4,3)) -ne $std){
+	for ($j=[int]($files[$files.Count-1].Substring(4,3))+1; $j -lt $std+1; $j++){
+		$n = $files[$files.Count-1].Substring(0,4) + ("{0:D3}" -f $j)
+		echo $n
+	}
+}	
 echo ''
